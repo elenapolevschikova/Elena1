681bc18537666109ca0d5cdbaaa75407907582f4
Urban <elenapolevschikova75@gmail.com>
2024-07-23 00:13:17 +0300

    теги HTML

---
+++ UrbanDjango/task3/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def cart(request):
+    return render(request, 'second_task/third_task/cart.html')
+
+
+
+def games(request):
+    return render(request, 'second_task/third_task/games.html')
+
+
+def platform(request):
+    return render(request, 'second_task/third_task/platform.html')
